010f2f73f7de69cee47c662df2dae1aa459fa770
andriawan24 <fawaznaufal23@gmail.com>
2025-10-06 22:29:29 +0700

    feature: Show dummy movies on recycler view

--- app/src/main/java/com/andriawan24/moviedb/ui/main/MainActivity.kt
+++ app/src/main/java/com/andriawan24/moviedb/ui/main/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.andriawan24.moviedb.ui.main
 
+import androidx.recyclerview.widget.GridLayoutManager
+import com.andriawan24.moviedb.adapter.MovieListAdapter
 import com.andriawan24.moviedb.base.BaseActivity
 import com.andriawan24.moviedb.databinding.ActivityMainBinding
+import com.andriawan24.moviedb.utils.decorator.GridSpacerDecorator
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
+
+    private val movieListAdapter = MovieListAdapter()
 
     override val binding: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
     override fun initUI() {
-        // TODO: Implement UI components
+        binding.apply {
+            rvMovies.apply {
+                adapter = movieListAdapter
+                layoutManager = GridLayoutManager(this@MainActivity, 2)
+                addItemDecoration(GridSpacerDecorator(2))
+            }
+        }
     }
 }
