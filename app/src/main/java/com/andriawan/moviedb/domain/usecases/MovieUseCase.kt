7a3d86b3ee7d6ef9730cd5bb80d96e39e8fe21e9
andriawan24 <fawaznaufal23@gmail.com>
2025-10-07 20:17:00 +0700

    fix: Remove unused imports

--- app/src/main/java/com/andriawan/moviedb/domain/usecases/MovieUseCase.kt
+++ app/src/main/java/com/andriawan/moviedb/domain/usecases/MovieUseCase.kt
@@ -3,12 +3,8 @@ package com.andriawan.moviedb.domain.usecases
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
-import androidx.paging.PagingSource
-import androidx.paging.PagingState
-import com.andriawan.moviedb.data.network.MovieAPI
 import com.andriawan.moviedb.data.repository.MovieRepository
 import com.andriawan.moviedb.domain.models.Movie
-import com.andriawan.moviedb.domain.models.PaginationMovie
 import kotlinx.coroutines.flow.Flow
 
 class MovieUseCase(private val movieRepository: MovieRepository) {
