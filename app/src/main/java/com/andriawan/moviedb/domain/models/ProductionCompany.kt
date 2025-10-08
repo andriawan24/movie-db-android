fecf65b9d0a7e46beab280ecc768a2e004b52a9a
andriawan24 <fawaznaufal23@gmail.com>
2025-10-08 08:23:24 +0700

    feature: Add production company model

---
+++ app/src/main/java/com/andriawan/moviedb/domain/models/ProductionCompany.kt
@@ -0,0 +1,8 @@
+package com.andriawan.moviedb.domain.models
+
+data class ProductionCompany(
+    val id: Int = 0,
+    val logoPath: String = "",
+    val name: String = "",
+    val originCountry: String = ""
+)
